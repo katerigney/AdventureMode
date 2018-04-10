e66f5695afd9705c1f6a41249486c91dfa6ddddc
Rigney <krigney@proforma.com>
2018-04-09 21:13:59 -0400

    Add project files.

---
+++ AdventureMode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
